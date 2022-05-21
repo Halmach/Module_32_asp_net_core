c61b209157ae7bd2ab3f13e94e1057695d85faee
Khalmatov <large000@yandex.ru>
2022-05-21 16:19:32 +0300

    Кастомные Middleware

---
+++ CoreStartApp/Middlewares/LoggingMidlleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CoreStartApp.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        ///  Middleware-компонент должен иметь конструктор, принимающий RequestDelegate
+        /// </summary>
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        ///  Необходимо реализовать метод Invoke  или InvokeAsync
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Для логирования данных о запросе используем свойста объекта HttpContext
+            Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+            // Строка для публикации в лог
+            string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+            // Путь до лога (опять-таки, используем свойства IWebHostEnvironment)
+            string logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Logs", "RequestLog.txt");
+            // Используем асинхронную запись в файл
+            await File.AppendAllTextAsync(logFilePath, logMessage);
+            //Передача запроса далее по конвейеру
+           await _next.Invoke(context);
+        }
+    }
+}
--- CoreStartApp/Startup.cs
+++ CoreStartApp/Startup.cs
@@ -1,3 +1,4 @@
+using CoreStartApp.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -29,14 +31,25 @@ namespace CoreStartApp
             }
 
             app.UseRouting();
+            app.UseMiddleware<LoggingMiddleware>();
+            //app.Use(async (context, next) =>
+            //{
+            //    // Строка для публикации в лог
+            //    string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+            //    // Путь до лога (опять-таки, используем свойства IWebHostEnvironment)
+            //    string logFilePath = Path.Combine(env.ContentRootPath, "Logs", "RequestLog.txt");
+            //    // Используем асинхронную запись в файл
+            //    await File.AppendAllTextAsync(logFilePath, logMessage);
+            //    await next.Invoke();
+            //});
 
             app.Map("/about", About);
-            app.Use(async (context, next) =>
-            {
-                // Для логирования данных о запросе используем свойста объекта HttpContext
-                Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
-                await next.Invoke();
-            });
+            //app.Use(async (context, next) =>
+            //{
+            //    // Для логирования данных о запросе используем свойста объекта HttpContext
+            //    Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+            //    await next.Invoke();
+            //});
 
             app.UseEndpoints(endpoints =>
             {
